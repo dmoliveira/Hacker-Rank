1681a59553c96479cb6a1660303ce00763ccfce6
dm.oliveira <dmztheone@gmail.com>
2015-01-30 23:20:59 -0200

    Solved Algorithms/Implementation/Sherlock and Arrays.

--- Algorithms/Implementation/sherlock_and_array.py
+++ Algorithms/Implementation/sherlock_and_array.py
@@ -1,15 +1,23 @@
-#!/usr/bin/env python
 import sys
 
 if __name__ == "__main__":
     for _ in range(int(sys.stdin.readline())):
         n = int(sys.stdin.readline())
         vector = list(map(int, sys.stdin.readline().split()))
-        
         exists_same_sum = 'NO'
-        for index in range(1, len(vector)-1):
-            if sum(vector[:index]) == sum(vector[index+1:]):
-                exists_same_sum = 'YES'
-                break
-                
-        print(exists_same_sum)
+        if n > 2:
+            a = sum(vector[:1])
+            b = sum(vector[2:])
+        
+            for index in range(1, len(vector)-1):
+                if a == b:
+                    exists_same_sum = 'YES'
+                    break
+                    
+                a += vector[index]
+                b -= vector[index+1]
+            print(exists_same_sum)
+        elif n == 1:
+            print('YES')
+        else:
+            print('NO')
