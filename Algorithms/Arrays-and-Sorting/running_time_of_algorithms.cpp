ac2dcfe96cf2e7db4dd8609b83727a2003645d80
dm.oliveira <dmztheone@gmail.com>
2015-02-04 21:11:11 -0200

    Solved Algorithms/Arrays and Sorting/Running time of the algorithms.

---
+++ Algorithms/Arrays-and-Sorting/running_time_of_algorithms.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+
+void insertionSort(int N, int arr[]) {
+    int i,j;
+    int value;
+    int shift = 0;
+    for(i=1;i<N;i++)
+    {
+        value=arr[i];
+        j=i-1;
+        while(j>=0 && value<arr[j])
+        {
+            arr[j+1]=arr[j];
+            j=j-1;
+            shift++;
+        }
+        arr[j+1]=value;
+    }
+    printf("%d",shift);
+}
+int main(void) {
+
+    int N;
+    scanf("%d", &N);
+    int arr[N], i;
+    for(i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    insertionSort(N, arr);
+
+    return 0;
+}
+
+
